8964df921e3c82df973ee521078fa6f58d089628
AbrarSyed <sacabrarsyed@gmail.com>
2015-02-17 14:24:17 -0600

    added testing for the srg and csv system vars

--- forge/src/main/java/com/example/examplemod/ExampleMod.java
+++ forge/src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,5 +1,8 @@
 package com.example.examplemod;
 
+import java.io.File;
+import java.util.Arrays;
+
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -14,12 +17,20 @@ public class ExampleMod
     public static final String TEST2 = "@TEST2@";
     
     @EventHandler
-    public void init(FMLInitializationEvent event)
+    public void init(FMLInitializationEvent event) throws Exception
     {
 		// some example code
         System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
 
         // buildcraft reference test
         //System.out.println("BuildCraft in colorBlind mode?  >> "+PowerHandler.Type.PIPE);
+
+        File srgDir = new File(System.getProperty("net.minecraftforge.gradle.GradleStart.srgDir"));
+        System.out.println("FILE: "+srgDir);
+        System.out.println(Arrays.toString(srgDir.list()));
+
+        File csvDir = new File(System.getProperty("net.minecraftforge.gradle.GradleStart.csvDir"));
+        System.out.println("FILE: "+csvDir);
+        System.out.println(Arrays.toString(csvDir.list()));
     }
 }
