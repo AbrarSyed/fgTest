206ded36fd4cfe93b3fc4e57d907364219c58710
AbrarSyed <sacabrarsyed@gmail.com>
2016-03-05 13:33:10 -0600

    updated tweaker testing project

--- tweaker/src/main/java/com/example/tweaker/ExampleTweaker.java
+++ tweaker/src/main/java/com/example/tweaker/ExampleTweaker.java
@@ -3,27 +3,38 @@ package com.example.tweaker;
 import java.io.File;
 import java.util.List;
 
+import com.google.common.collect.Lists;
 import net.minecraft.launchwrapper.ITweaker;
 import net.minecraft.launchwrapper.LaunchClassLoader;
 
-public class TestTweaker implements ITweaker {
+public class ExampleTweaker implements ITweaker
+{
+    private List<String> args;
 
     @Override
-    public void acceptOptions(List<String> args, File gameDir, File assetsDir, String profile) {}
+    public void acceptOptions(List<String> args, File gameDir, File assetsDir, String profile)
+    {
+        // stupid launch wrapper...
+        this.args = Lists.newArrayList(args);
+        this.args.add("--version");
+        this.args.add(profile);
+    }
 
     @Override
-    public void injectIntoClassLoader(LaunchClassLoader classLoader) {
+    public void injectIntoClassLoader(LaunchClassLoader classLoader)
+    {
         System.out.println("Hello World.");
     }
 
     @Override
-    public String[] getLaunchArguments() {
-        return new String[0];
-    }
-
-    @Override
-    public String getLaunchTarget() {
+    public String getLaunchTarget()
+    {
         return "net.minecraft.client.main.Main";
     }
 
+    @Override
+    public String[] getLaunchArguments()
+    {
+        return this.args.toArray(new String[this.args.size()]);
+    }
 }
